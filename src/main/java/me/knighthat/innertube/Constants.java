3073a450b9bcc74fadfb93f78137bc75138d639a
knighthat <knightchanelgaming@gmail.com>
2024-10-22 10:03:28 -0500

    format code

--- src/main/java/me/knighthat/innertube/Constants.java
+++ src/main/java/me/knighthat/innertube/Constants.java
@@ -4,12 +4,15 @@ import org.jetbrains.annotations.NotNull;
 
 public interface Constants {
 
+    // START: Static fields/functions
     @NotNull
     String[] SUPPORTED_LANGUAGES = { "en-US" };
     @NotNull
-    String[] SUPPORTED_ENCODINGS = { };
+    String[] SUPPORTED_ENCODINGS = {};
     @NotNull
-    String CONTENT_TYPE = "application/json";
+    String   CONTENT_TYPE        = "application/json";
     @NotNull
-    String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.3";
+    String   USER_AGENT          = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.3";
+
+// END: Static fields/functions
 }
